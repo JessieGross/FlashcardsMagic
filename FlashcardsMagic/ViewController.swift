9208429898d8f6f0b1c69ddde7d854e593423f94
Jessie G <z3r0@jessies-macbook-pro.local>
2018-10-12 21:23:07 -0700

    Initial Commit

---
+++ FlashcardsMagic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashcardsMagic
+//
+//  Created by Jessie G on 10/12/18.
+//  Copyright © 2018 Jessie Gross. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
